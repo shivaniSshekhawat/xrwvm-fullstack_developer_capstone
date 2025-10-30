e04ee70bf5838ce9f942db909c27ebcf5cbb482d
shivaniSshekhawat <140176483+shivaniSshekhawat@users.noreply.github.com>
2025-10-30 23:32:43 +0530

    Add admin classes for CarMake and CarModel

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 # Inline class for CarModel
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 1  # Number of empty forms to show by default
+
 
 # Admin class for CarMake
 class CarMakeAdmin(admin.ModelAdmin):
@@ -12,11 +14,13 @@ class CarMakeAdmin(admin.ModelAdmin):
     list_display = ('name', 'description')  # adjust based on your fields
     search_fields = ('name',)
 
+
 # Admin class for CarModel
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'car_make', 'type', 'year')
     list_filter = ('type', 'year')
     search_fields = ('name',)
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
