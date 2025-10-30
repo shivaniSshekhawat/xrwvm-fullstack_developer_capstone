cb21185f9b7735699e92531532070381b5b794e2
shivaniSshekhawat <140176483+shivaniSshekhawat@users.noreply.github.com>
2025-10-30 23:34:10 +0530

    Refactor CarMake and CarModel definitions

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,37 +1,30 @@
-# Uncomment the following imports before adding the Model code
-
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 
+# Car Make model
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    country = models.CharField(max_length=100, blank=True, null=True)  # Optional extra field
+    country = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True
+    )  # Optional extra field
 
     def __str__(self):
         return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
-# - Many-To-One relationship to Car Make model (One Car Make has many
-# Car Models, using ForeignKey field)
-# - Name
-# - Type (CharField with a choices argument to provide limited choices
-# such as Sedan, SUV, WAGON, etc.)
-# - Year (IntegerField) with min value 2015 and max value 2023
-# - Any other fields you would like to include in car model
-# - __str__ method to print a car make object
+
+# Car Model model
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )  # Many-to-One
     dealer_id = models.IntegerField()  # Refers to dealer in Cloudant database
     name = models.CharField(max_length=100)
 
@@ -42,7 +35,11 @@ class CarModel(models.Model):
         ('TRUCK', 'Truck'),
         ('COUPE', 'Coupe'),
     ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(
+        max_length=10,
+        choices=CAR_TYPES,
+        default='SUV'
+    )
 
     year = models.IntegerField(
         validators=[
